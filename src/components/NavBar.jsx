ed0dfcb27129d99fc3938b562f7a438d29c9e59d
Tal <trimoni8@gmail.com>
2023-02-02 13:36:31 -0500

    links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function NavBar() {
   return (
     <div className='navbar'> 
       <div className='toggleButton'>
         <button></button>
+      </div>
+      <div className='links'>
+        <Link></Link>
       </div>
     </div>
   )
