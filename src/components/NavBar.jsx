cdadfb85a9a53ef1bd6f70798364a2d5152faef2
Tal <trimoni8@gmail.com>
2023-02-02 13:50:38 -0500

    useeffect

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,17 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import {Link, useLocation} from 'react-router-dom'
 import '../styles/Navbar.css'
 import ReorderIcon from '@material-ui/icons/Reorder'
 
 function NavBar() {
-    const [expandNavbar, setExpandNavbar] = useState(false)
+  const [expandNavbar, setExpandNavbar] = useState(false)
+
+  const location = useLocation
+
+  useEffect(() => {
+    setExpandNavbar(false)
+  }, [location])
+
   return (
     <div className='navbar' id={expandNavbar ? 'open' : 'close'}> 
       <div className='toggleButton'>
