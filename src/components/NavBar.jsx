a27f80214aacb8d2f66eabbf6011db4a53bdbd1c
Tal <trimoni8@gmail.com>
2023-02-02 13:35:50 -0500

    button

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='navbar'> 
+      <div className='toggleButton'>
+        <button></button>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
