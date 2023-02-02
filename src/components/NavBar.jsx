25404896bacf17d9738b1d106f3d0389f0ec339f
Tal <trimoni8@gmail.com>
2023-02-02 13:46:52 -0500

    state

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import '../styles/Navbar.css'
 import ReorderIcon from '@material-ui/icons/Reorder'
 
 function NavBar() {
+    const [expandNavbar, setExpandNavbar] = useState(false)
   return (
     <div className='navbar'> 
       <div className='toggleButton'>
-        <button> <ReorderIcon/> </button>
+        <button onClick={() => {setExpandNavbar((prev) => !prev)}}> <ReorderIcon/> </button>
       </div>
       <div className='links'>
         <Link to='/'> Home </Link>
