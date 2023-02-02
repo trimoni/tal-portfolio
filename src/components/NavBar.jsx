0b6b26541a857db29d9d05313a54506f2f448dbc
Tal <trimoni8@gmail.com>
2023-02-02 13:45:36 -0500

    reorder

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import '../styles/Navbar.css'
+import ReorderIcon from '@material-ui/icons/Reorder'
 
 function NavBar() {
   return (
     <div className='navbar'> 
       <div className='toggleButton'>
-        <button></button>
+        <button> <ReorderIcon/> </button>
       </div>
       <div className='links'>
         <Link to='/'> Home </Link>
