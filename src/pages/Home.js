68a3b998bc1753ab47dd377f4543b61d5d1dd046
Tal <trimoni8@gmail.com>
2023-02-02 14:03:54 -0500

    home page icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import EmailIcon from '@material-ui/icons/Email'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'>
+          <h2> Hi, My Name is Tal</h2>
+          <div className='prompt'> 
+          <p> A Software Developer with a passion for learning and creating.
+          </p>
+          <LinkedInIcon/>
+          <GitHubIcon/>
+          <EmailIcon/>
+          </div>
+        <div className='skills'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
