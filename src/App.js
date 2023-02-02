22f4fee73b2cfbc67c9ae930e7fbf368a044a3c5
Tal <trimoni8@gmail.com>
2023-02-02 13:30:36 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/projects' element={<Projects />}></Route>
+          <Route path='/experience' element={<Experience />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
