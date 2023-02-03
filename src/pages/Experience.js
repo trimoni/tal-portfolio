1e54a78f0ac2bef863e5ff7a5ccabe5e66c2bad8
Tal <trimoni8@gmail.com>
2023-02-03 09:35:24 -0500

    icon

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
+import SchoolIcon from '@material-ui/icons/School'
 
 function Experience() {
   return (
     <div className='experience'>
       <VerticalTimeline lineColor='#3e497a'>
-        <VerticalTimelineElement className='vertical-timeline-element--education'></VerticalTimelineElement>
+        <VerticalTimelineElement
+        className='vertical-timeline-element--education'
+        date="2015 - 2019"
+        iconStyle={{background: '#3e497a', colo: '#fff'}}
+        icon={<SchoolIcon/>}
+        ></VerticalTimelineElement>
       </VerticalTimeline>
     </div>
   )
