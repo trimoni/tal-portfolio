4eb256360b40a575b1cb195f3a314f01ea4d4647
Tal <trimoni8@gmail.com>
2023-02-03 09:45:54 -0500

    project Item

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem'
+import Img 
 
 function Projects() {
   return (
     <div className='projects'>
       <h1> My Personal Projects</h1>
       <div className='projectList'>
-        
+        <ProjectItem/>
+        <ProjectItem/>
       </div>
     </div>
   )
