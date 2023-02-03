f9771e6e595fa16e23276ad1fbe00ae82bb4da34
Tal <trimoni8@gmail.com>
2023-02-03 09:59:09 -0500

    add projects

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,67 @@
+import Proj1 from '../assets/color-connect-four2.jpg'
+import Proj2 from '../assets/tictactoe.jpg'
+import Proj3 from '../assets/event-planner.jpg'
+import Proj4 from '../assets/myProps.jpg'
+import Proj5 from '../assets/pokemoncollector.png'
+import Proj6 from '../assets/wordle.png'
+import Proj7 from '../assets/hat.jpg'
+
+const projects = [
+  {
+    title: 'ColorFul Connect Four',
+    description: `HTML, CSS & JavaScript`,
+    image: Proj1,
+    repositoryLink: 'https://github.com/trimoni/Connect-Four-Unit-1.git',
+    depositoryLink: 'https://colorfulconnectfour.netlify.app/'
+  },
+  
+  {
+    title: 'Weather Tic-Tac-Toe',
+    description: `HTML, CSS & JavaScript`,
+    image: Proj2,
+    repositoryLink: 'https://github.com/trimoni/ttt-weekend.git',
+    depositoryLink: 'https://weather-tic-tac-toe.netlify.app/'
+  },
+  
+  {
+    title: 'Event Planner',
+    description: `Express & Node.Js`,
+    image: Proj3,
+    repositoryLink: 'https://github.com/trimoni/Event-Planning.git',
+    depositoryLink: 'https://eventplanner-rimoni.fly.dev/'
+  },
+  
+  {
+    title: 'myProps',
+    description: `React & CSS`,
+    image: Proj4,
+    repositoryLink: 'https://github.com/trimoni/myProps-front-end.git',
+    depositoryLink: 'https://mypropsmanagement.netlify.app/'
+  },
+  
+  {
+    title: 'Pokemon Card Collector',
+    description: `Python & Django`,
+    image: Proj5,
+    repositoryLink: 'https://github.com/trimoni/pokemon-card-collector.git',
+    depositoryLink: 'https://pokemon-card-collector.herokuapp.com/'
+  },
+  
+  {
+    title: 'Wordle',
+    description: `HTML, CSS & JavaScript`,
+    image: Proj6,
+    repositoryLink: 'https://github.com/trimoni/wordle-js.git',
+    depositoryLink: 'https://wordle-free.netlify.app/'
+  },
+  
+  {
+    title: 'Hat-Shop',
+    description: `React & CSS`,
+    image: Proj7,
+    repositoryLink: 'https://github.com/trimoni/shopping-cart-react.git',
+    depositoryLink: 'https://ralph-lauren-hat-store.netlify.app/'
+  }
+  ]
+  
+  export {projects}
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import '../styles/Projects.css'
 import ProjectItem from '../components/ProjectItem'
-import Proj1 from '../assets/color-connect-four2.jpg'
-import Proj2 from '../assets/tictactoe.jpg'
+// import Proj1 from '../assets/color-connect-four2.jpg'
+// import Proj2 from '../assets/tictactoe.jpg'
 
 function Projects() {
   return (
     <div className='projects'>
       <h1> My Personal Projects</h1>
       <div className='projectList'>
-        <ProjectItem name='Connect Four' image={Proj1} />
-        <ProjectItem name='Tic Tac Toe' image={Proj2}/>
+        {/* <ProjectItem name='Connect Four' image={Proj1} />
+        <ProjectItem name='Tic Tac Toe' image={Proj2}/> */}
       </div>
     </div>
   )
